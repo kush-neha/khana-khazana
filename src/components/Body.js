d68b4615eb85a143e4e86811edc00b56f88d5179
neha kushwah <neha.kushwah@logicmonitor.com>
2024-03-15 14:27:07 +0530

    left-panel-changes

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,23 +2,13 @@ import React, { useState } from "react";
 import "../Styles/Body.css";
 
 export const Body = () => {
-  // State to track whether the list is open or not
-  const [isOpen, setIsOpen] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+  const [showDescription, setShowDescription] = useState("");
+
   const [showFetchButton, setShowFetchButton] = useState(false); // State for fetch button visibility
-  // State to manage the visibility of textboxes
   const [textBoxesVisibility, setTextBoxesVisibility] = useState({});
 
-  // Function to handle input change
   const handleInputChange = (val) => {
-    setInputValue(val);
-    // Show fetch button if input value is not empty
     setShowFetchButton(val.trim() !== "");
-  };
-
-  // Function to toggle the state of isOpen
-  const toggleList = () => {
-    setIsOpen(!isOpen);
   };
 
   // Function to toggle the visibility of a specific textbox
@@ -29,48 +19,49 @@ export const Body = () => {
     }));
   };
 
+  const tryItOut = () => {
+    setShowFetchButton(true);
+  }
+
+  const showDescriptionFunction = () => {
+    setShowDescription(true);
+  }
+
+  const removeDescriptionFunction = () => {
+    setShowDescription(false);
+    setShowFetchButton(false);
+  }
+
   const data = [
     {
       heading: "Rtp API",
       subheading: "RTP response Status Controller",
       textBox: "textbox1",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
     },
     {
       heading: "Settlement Overview API",
       subheading: "Settlement Overview Controller",
-      textBox: "textbox5",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
+      textBox: "textbox2",
     },
     {
       heading: "access-role-controller",
       subheading: "Access Role Controller",
-      textBox: "textbox6",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
+      textBox: "textbox3",
     },
     {
       heading: "access-user-rest-controller",
       subheading: "Access User Rest Controller",
-      textBox: "textbox2",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
+      textBox: "textbox4",
     },
     {
       heading: "activation-rest-controller",
       subheading: "Activation Rest Controller",
-      textBox: "textbox3",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
+      textBox: "textbox5",
     },
     {
       heading: "cass-rest-controller",
       subheading: "Cass Rest Controller",
-      textBox: "textbox4",
-      paragraph:
-        "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",
+      textBox: "textbox6",
     },
   ];
 
@@ -82,26 +73,48 @@ export const Body = () => {
           <ul key={newData.textBox} className="list">
             {/* Toggle the visibility of corresponding textbox on list item click */}
             <li onClick={() => toggleTextBox(newData.textBox)}>
-             {newData.heading} <p>{newData.subheading}r</p>
+              {newData.heading} <p>{newData.subheading}r</p>
             </li>
-            <div className="spanAndTextbox">
-              <input
-                type="text"
-                className="textbox"
-                style={{
-                  display: textBoxesVisibility[newData.textBox] ? "block" : "none",
-                }}
-                placeholder={newData.textBox}
-                // value={textboxes.textbox1}
-                onChange={(e) => handleInputChange(e.target.value)}
-              />
-              {textBoxesVisibility[newData.textBox] && (
-                <span>{newData.paragraph}</span>
-              )}
-            </div>
-            {showFetchButton && textBoxesVisibility[newData.textBox] && (
-              <button className="fetchButton">Fetch</button>
+            <div
+              style={{
+                display: textBoxesVisibility[newData.textBox]
+                  ? "block"
+                  : "none",
+              }}
+            >
+              <div className="parameters-container">
+                <h3>Parameters</h3>
+                <button style={{marginLeft: '84%'}} className="tryButton" onClick = {tryItOut}>Try it out</button>
+              </div>
+              <table>
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <td>participant Id
+                    <h5>string</h5>
+                    (path)
+                  </td>
+                  <td>
+                    participant Id
+                    <input
+                      type="text"
+                      className="textbox"
+                      placeholder={newData.textBox}
+                      // value={textboxes.textbox1}
+                      onChange={(e) => handleInputChange(e.target.value)}
+                    />
+                  </td>
+                </tbody>
+              </table>
+              {showFetchButton && textBoxesVisibility[newData.textBox] && (
+              <div className="button-container"><button onClick={removeDescriptionFunction}>Clear</button><button onClick={showDescriptionFunction}>Fetch</button></div>
             )}
+              {showDescription && <p> "Paragraphs are the building blocks of papers. Many students define paragraphs in terms of length: a paragraph is a group of at least five sentences, a paragraph is half a page long, etc. In reality, though, the unity and coherence of ideas among sentences is what constitutes a paragraph. A paragraph is defined as “a group of sentences or a single sentence that forms a unit” (Lunsford and Connors 116). Length and appearance do not determine whether a section in a paper is a paragraph. For instance, in some styles of writing, particularly journalistic styles, a paragraph can be just one sentence long. Ultimately, a paragraph is a sentence or group of sentences that support one main idea. In this handout, we will refer to this as the “controlling idea,” because it controls what happens in the rest of the paragraph.",</p>}
+            </div>
           </ul>
         ))}
       </div>
